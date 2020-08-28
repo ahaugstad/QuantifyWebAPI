3043b484c3c8b551ade4b72388bcfcb2423be230
Alex Haugstad <Alex.Haugstad@apigroupinc.us>
2020-08-28 14:28:36 -0500

    Moved Address and Contact classes to shared class, since they are general classes we can apply to multiple data entities.

---
+++ Classes/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace QuantifyWebAPI.Classes
+{
+    public class Address
+    {
+        public string addressTypeCode { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string country { get; set; }
+    }
+
+    public class Contact
+    {
+        public string contact_name { get; set; }
+        public string contact_phone { get; set; }
+        public string contact_email { get; set; }
+    }
+}
