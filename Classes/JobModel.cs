98e5e6c8f4cd961ad2ecc851b161713a1d6da3d5
Alex Haugstad <Alex.Haugstad@apigroupinc.us>
2020-09-01 17:30:38 -0500

    1) Created JobModel class to define Jobs JSON profile to be passed.
    2) Coded most of the field assignments for the Jobs JSON object, pending a few questions.

---
+++ Classes/JobModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace QuantifyWebAPI.Classes
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class JobRootClass
+    {
+        public string entity { get; set; }
+        public JobData JobData { get; set; }
+    }
+
+    public class JobData
+    {
+        public string job_id { get; set; }
+        public string job_name { get; set; }
+        public string site_name { get; set; }
+        public string site_address1 { get; set; }
+        public string site_city { get; set; }
+        public string site_state { get; set; }
+        public string site_zip { get; set; }
+        public string customer_id { get; set; }
+        public string sales_taxable { get; set; }
+        public string sales_tax_code { get; set; }
+        public string job_start_date { get; set; }
+        public string job_estimated_end_date { get; set; }
+        public string department { get; set; }
+        public string job_type { get; set; }
+        public string retainage_percent { get; set; }
+    }
+
+    public class JobResponseObj
+    {
+        public string status { get; set; }
+        public List<string> errorList { get; set; }
+        public JobResponseObj()
+        {
+            errorList = new List<string>();
+        }
+    }
+}
--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -44,22 +44,53 @@ namespace QuantifyWebAPI.Controllers
 
             QuantHelper.QuantifyLogin();
 
-            // Get list of all jobsites
+            //***** Get list of all jobsites *****
             StockingLocationList all_jobsites = StockingLocationList.GetJobsites(false, JobTreeNodeDisplayType.Name, Guid.Empty);
 
-            // Filter down to only jobs that have been updated since the last run date
+            //***** Loop through only jobs that have been updated since the last run date, sending each through as own call *****
             StockingLocationList jobsites = StockingLocationList.NewStockingLocationList();
             foreach (StockingLocationListItem jobsiteItem in all_jobsites)
             {
                 StockingLocation jobsite = StockingLocation.GetStockingLocation(jobsiteItem.StockingLocationID, false);
                 if(jobsite.VersionStamp[jobsite.VersionStamp.Length] != parmVersionStamp) 
                 {
-                    jobsites.Add(jobsiteItem);
+                    //***** Populate Fields *****
+                    JobRootClass myDeserializedClass = new JobRootClass();
+                    myDeserializedClass.JobData.job_id = jobsite.Number;
+                    myDeserializedClass.JobData.job_name = jobsite.Description;
+                    myDeserializedClass.JobData.site_name = jobsite.Name;
+                    //***** Look up address to get individual address fields *****
+                    // TO DO: how to do this? or is job address info accessible another way?
+                    //Avontus.Rental.Library.Address jobAddress = Avontus.Rental.Library.Address.GetAddress(jobsite.ShippingAddress)
+                    myDeserializedClass.JobData.site_address1 = jobsite.ShippingAddress;
+                    //***** Look up customer to get customer ID *****
+                    BusinessPartner jobCustomer = BusinessPartner.GetBusinessPartnerByName(jobsite.CustomerName);
+                    myDeserializedClass.JobData.customer_id = jobCustomer.Name;
+                    //***** Identify if job is sales taxable, and if so, assign a use tax code *****
+                    if (jobsite.ConsumablesTaxable) 
+                    { 
+                        myDeserializedClass.JobData.sales_taxable = "Y";
+                        // TO DO: will need to map this in Boomi?
+                        myDeserializedClass.JobData.sales_tax_code = jobsite.JobTax1.Name; 
+                    } 
+                    else 
+                    { 
+                        myDeserializedClass.JobData.sales_taxable = "N";
+                        myDeserializedClass.JobData.sales_tax_code = "";
+                    }
+                    myDeserializedClass.JobData.job_start_date = jobsite.StartDate;
+                    myDeserializedClass.JobData.job_estimated_end_date = jobsite.StopDate;
+                    myDeserializedClass.JobData.department = "20";
+                    myDeserializedClass.JobData.job_type = "S";
+                    // TO DO: where is the retention percentage?
+                    // myDeserializedClass.JobData.retainage_percent = ?;
+                    
+                    // TO DO: serialize class to pass back to Boomi
+                    //***** Serialize class into JObject to pass to Boomi ******
+                    //JObject myJsonObject = myDeserializedClass;
                 }
             }
-
             return "value";
         }
-
     }
 }
